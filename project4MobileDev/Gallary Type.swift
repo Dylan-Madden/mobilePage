3b495b422093f18a77f7e61ad63eafdb5528d1d7
Dylan-Madden <79376614+Dylan-Madden@users.noreply.github.com>
2025-03-05 08:18:10 -0500

    Add files via upload

---
+++ project4MobileDev/Gallary Type.swift
@@ -0,0 +1,14 @@
+//
+//  Gallary Type.swift
+//  project4MobileDev
+//
+//  Created by Dylan Madden on 2/26/25.
+//
+
+import SwiftUI
+
+struct GalleryItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let subtitle: String
+}
